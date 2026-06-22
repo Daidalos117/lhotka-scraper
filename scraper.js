171754073ae432e285bfea11ff5e8c4db1082c2a
Roman Rajchert <roman.rajchert@vodafone.com>
2026-06-22 19:28:34 +0200

    chore: try catch

--- scraper.js
+++ scraper.js
@@ -7,24 +7,32 @@ const pb = new PocketBase('http://pocketbase-zgvssk3wql5l59p3dgc6hequ.188.245.41
 
 const formatTemperature = (tempString) => tempString.trim().replace('°C', '').replace(",", ".");
 
-async function scrape() {
-  await pb.collection('users').authWithPassword('lhotka_creator@yopmail.com', 'VoHA#5f4GMTDiDk7zCn28xiwTiPKiWGN*0%*jpfV3e4Otzlr!M');
+async function runScraper() {
+  try {
+    await pb.collection('users').authWithPassword('lhotka_creator@yopmail.com', 'VoHA#5f4GMTDiDk7zCn28xiwTiPKiWGN*0%*jpfV3e4Otzlr!M');
 
-  const { data } = await axios.get('https://www.koupaliste-lhotka.cz/');
-  const $ = cheerio.load(data);
+    const { data } = await axios.get('https://www.koupaliste-lhotka.cz/');
+    const $ = cheerio.load(data);
 
-  const info = {
-    waterTemp: formatTemperature($('.header-obsazenost__inner:contains("Teplota vody") strong').text()),
-    airTemp: formatTemperature($('.header-obsazenost__inner:contains("Teplota vzduchu") strong').text()),
-    occupancy: parseInt($('.header-obsazenost__inner:contains("obsazenost") strong').text().trim().split('/')[0].trim(), 10),
-    status: $('.opening-hours p').text().trim(),
-  };
+    const info = {
+      waterTemp: formatTemperature($('.header-obsazenost__inner:contains("Teplota vody") strong').text()),
+      airTemp: formatTemperature($('.header-obsazenost__inner:contains("Teplota vzduchu") strong').text()),
+      occupancy: parseInt($('.header-obsazenost__inner:contains("obsazenost") strong').text().trim().split('/')[0].trim(), 10),
+      status: $('.opening-hours p').text().trim(),
+    };
 
-  const record = await pb.collection('lhotka').create(info);
+    const record = await pb.collection('lhotka').create(info);
 
-  console.log(JSON.stringify(info, null, 2));
-  console.log('----------------------------------------');
-  console.log(record);
+    console.log(JSON.stringify(info, null, 2));
+    console.log('----------------------------------------');
+    console.log(record);
+
+    console.log('Scraping úspešne dokončený.');
+    process.exit(0);
+  } catch (error) {
+    console.error('Chyba:', error);
+    process.exit(1);
+  }
 }
 
-scrape().catch(console.error);
+runScraper();
